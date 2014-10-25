053daf1e2f00beadeb335575c185e0565dc69d14
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-10-25 19:17:25 +0900

    Create PreferenceKey

---
+++ app/src/main/java/com/operamdo/tweetassist/constants/PreferenceKey.java
@@ -0,0 +1,8 @@
+package com.operamdo.tweetassist.constants;
+
+public final class PreferenceKey {
+    private PreferenceKey() {
+    }
+
+    public static final String DEFAULT_TWEET_TEXT = "default_tweet_text";
+}
